7846cda4d2f7367a79a5284ffb787e30074f9538
Andres B <andresblasiche@gmail.com>
2023-05-21 01:13:53 -0300

    Funcion myRequest, funcion pagination

--- hiper/spiders/prueba_spider.py
+++ hiper/spiders/prueba_spider.py
@@ -18,6 +18,7 @@ class HiperSpider(scrapy.Spider):
 			self.sucursal = sucursal
 		else:
 			self.sucursal = 1
+		self.items_per_req = 30
 
 
 	allowed_domains = ["www.hiperlibertad.com.ar"]
@@ -27,21 +28,25 @@ class HiperSpider(scrapy.Spider):
 	]
 
 
-	def start_requests(self):
-		url = self.start_urls[0]
+	def myRequest(self, url, callback):
 		if self.proxy:
-			yield scrapy.Request(
+			return scrapy.Request(
 				url,
 				meta={'proxy':self.proxy},
-				callback=self.parse_urls
+				callback=callback
 			)
 		else:
-			yield scrapy.Request(url, callback=self.parse_urls)
+			return scrapy.Request(url, callback=callback)
+
+
+	def start_requests(self):
+		url = self.start_urls[0]
+		yield self.myRequest(url, self.parse_urls)
 
 
 	def get_urls(self, json_cat, lista):
 		path_api = "/api/catalog_system/pub/products/search"
-		arg_api = "?O=OrderByTopSaleDESC&_from=0&_to=30&ft"
+		arg_api = f"?_from=0&_to={self.items_per_req - 1}"
 		arg_suc = f"&sc={self.sucursal}"
 		for dic in json_cat:
 			if dic.get("hasChildren"):
@@ -58,38 +63,43 @@ class HiperSpider(scrapy.Spider):
 		json_resp = json.loads(response.body)
 		self.get_urls(json_resp, list_urls)
 		for url in list_urls:
-			if self.proxy:
-				yield scrapy.Request(
-					url,
-					meta={'proxy':self.proxy},
-					callback=self.parse_urls
-				)
-			else:
-				yield scrapy.Request(url, callback=self.parse)
+			yield self.myRequest(url, self.parse)
+
+
+	def pagination(self, url):
+		url2 = url.split("=")
+		inicio = int(url2[1].split("&")[0]) + self.items_per_req
+		fin = int(url2[2].split("&")[0]) + self.items_per_req
+		url2[1] = f"{inicio}&_to"
+		url2[2] = f"{fin}&sc"
+		url2 = "=".join(url2)
+		return url2
 
 
 	def parse(self, response):
 		json_resp = json.loads(response.body)
-		cargador = ItemLoader(item=HiperItem(), response=response)
-		#for prod in json_resp:
-		#Por ahora imprimo solo el primer producto
-		prod = json_resp[0]
-		cargador.add_value("nombre", prod["productName"])
-		cargador.add_value(
-			"precio_reg",
-			prod["items"][0]["sellers"][0]["commertialOffer"]["ListPrice"]
-		)
-		cargador.add_value(
-			"precio_pub",
-			prod["items"][0]["sellers"][0]["commertialOffer"]["Price"]
-		)
-		cargador.add_value("categoria", prod["categories"][0])
-		cargador.add_value("sku", prod["items"][0]["itemId"])
-		cargador.add_value("url_prod", prod["link"])
-		cargador.add_value(
-			"stock",
-			prod["items"][0]["sellers"][0]\
-				["commertialOffer"]["AvailableQuantity"]
-		)
-		cargador.add_value("descripcion", prod["description"])
-		return cargador.load_item()
+		
+		for prod in json_resp:
+			cargador = ItemLoader(item=HiperItem(), response=response)
+			cargador.add_value("nombre", prod["productName"])
+			cargador.add_value(
+				"precio_reg",
+				prod["items"][0]["sellers"][0]["commertialOffer"]["ListPrice"]
+			)
+			cargador.add_value(
+				"precio_pub",
+				prod["items"][0]["sellers"][0]["commertialOffer"]["Price"]
+			)
+			cargador.add_value("categoria", prod["categories"][0])
+			cargador.add_value("sku", prod["items"][0]["itemId"])
+			cargador.add_value("url_prod", prod["link"])
+			cargador.add_value(
+				"stock",
+				prod["items"][0]["sellers"][0]\
+					["commertialOffer"]["AvailableQuantity"]
+			)
+			#cargador.add_value("descripcion", prod["description"])
+			yield cargador.load_item()
+		#Paginación
+		if (len(json_resp) == self.items_per_req):
+			yield self.myRequest(self.pagination(response.url), self.parse)
